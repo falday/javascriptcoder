73132cf797c3a0f3a4af6fb3302c0e0f15faff0c
Desafio-js <aldayfranco@gmail.com>
2022-07-04 21:14:38 -0300

    desafio

--- main.js
+++ main.js
@@ -10,11 +10,7 @@ for (let DiasDeTrabajo = 0; DiasDeTrabajo < 7; DiasDeTrabajo++)
  {
     horas = prompt("ingrese la cantidad de horas trabajadas por dia en la semana, al finalizar ingrese:  0 ");
     if (horas == 0)
-         {
-          break
-         }
-         else
-            {
+    {break}
                  if (horas <= 14)
 
                     { horastrabajadas.push(horas);
@@ -28,10 +24,18 @@ for (let DiasDeTrabajo = 0; DiasDeTrabajo < 7; DiasDeTrabajo++)
     
                     }
     }
- }
 
+ document.write("los dias trabajados son " + jornadas);
+ document.write(". las Horas trabajadas son "+ horastrabajadas);
+ document.write(". la cantida de horas son " + totalHoras);
 
- document.write("los dias trabajados son " + jornadas)
- document.write(". las Horas trabajadas son "+ horastrabajadas)
- document.write(". la cantida de horas son " + totalHoras)
+promedio = parseFloat(totalHoras/jornadas);
+ alert("Las horas trabajadas en promedio por dia son: " + promedio);
+let valorHora = prompt("ingrese el valor de la hora");
+valorHora = valorHora*totalHoras;
+let viaticos = prompt("ingrese monto de viaticos diarios");
+viaticos = viaticos*jornadas
+let paga= valorHora + viaticos;
+document.write("total por horas de trabajo $",+valorHora, " por viaticos $", +viaticos," El PAGO DE LA SEMANA ES :$" + paga  );
+
 
